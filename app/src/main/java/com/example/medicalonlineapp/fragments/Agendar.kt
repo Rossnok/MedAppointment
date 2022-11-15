68012ee688e1010a038fc5d4d01f1dc0142683d5
Rossnok <Onlyrossnok@gmail.com>
2022-11-14 20:08:37 -0600

    Creacion del datapicker para seleccionar la fecha en calendario, funciones basicas de un fragment

--- app/src/main/java/com/example/medicalonlineapp/fragments/Agendar.kt
+++ app/src/main/java/com/example/medicalonlineapp/fragments/Agendar.kt
@@ -5,29 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.medicalonlineapp.R
+import kotlinx.android.synthetic.main.fragment_agendar.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Agendar.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class Agendar : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    }
+
+    private fun mostrarMensaje(){
+        Toast.makeText(layoutInflater.context.applicationContext, "este es el mensaje del boton", Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreateView(
@@ -36,25 +30,29 @@ class Agendar : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_agendar, container, false)
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Agendar.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Agendar().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        agendarCita.setOnClickListener{//aqui dentro es donde pueden ir las llamadas de los botones, los set on click listener y de mas
+            mostrarMensaje()
+        }
+
+        txtFechaCita.setOnClickListener{
+            showDialogPicker()
+        }
     }
+
+    private fun showDialogPicker() {
+        val dataPicker = DatePickerFragment {day, month, year -> onDateSelected(day, month, year)}
+        fragmentManager?.let { dataPicker.show(it,"Fecha de la cita") }
+    }
+
+     private fun onDateSelected(day:Int, month:Int, year:Int){
+        txtFechaCita.setText("$day/${month+1}/$year")
+    }
+
+
 }
