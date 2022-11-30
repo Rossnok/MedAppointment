f84c41926eadcf13d2c6f45e5382b3f5de580860
Rossnok <Onlyrossnok@gmail.com>
2022-11-29 22:45:02 -0600

    Comprobaciones en agendar cita, estructura del registro de medicos en MainActivity2.kt

--- app/src/main/java/com/example/medicalonlineapp/MainActivity2.kt
+++ app/src/main/java/com/example/medicalonlineapp/MainActivity2.kt
@@ -1,18 +1,134 @@
 package com.example.medicalonlineapp
 
-import android.content.Intent
+
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import com.example.medicalonlineapp.principalView.PrincipalView
+import android.util.Log
+import android.widget.Toast
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
+import com.example.medicalonlineapp.fragments.Agendar
+import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.fragment_agendar.*
+import org.json.JSONObject
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity2 : AppCompatActivity() {
+
+    val hosting: String = "https://rossworld.000webhostapp.com/"
+    private var progresAsyncTask: ProgressAsyncTask? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
         supportActionBar!!.hide()
+
+        registerButton.setOnClickListener{
+            if(!username.text.isNullOrEmpty() && !password.text.isNullOrEmpty() && !name.text.isNullOrEmpty()
+                && !cedule.text.isNullOrEmpty() && !phone.text.isNullOrEmpty()){
+
+                val json = JSONObject()
+
+                progresAsyncTask = progresAsyncTask
+                progresAsyncTask!!.execute("POST", hosting + "InsertMedico.php", json.toString())
+
+            }else{
+                Toast.makeText(applicationContext, "Campo vacio, compruebe que todos los campos se hayan llenado correctamente", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
+
+    inner class ProgressAsyncTask: AsyncTask<String, Unit, String>(){
+
+        val TIMEOUT = 50000
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg params: String): String? {
+
+            val url = URL(params[1])
+            val httpClient = url.openConnection() as HttpURLConnection
+            httpClient.readTimeout = TIMEOUT
+            httpClient.connectTimeout = TIMEOUT
+            httpClient.requestMethod = params[0]
+
+            if(params[0] == "POST"){
+                httpClient.instanceFollowRedirects = false
+                httpClient.doOutput = true
+                httpClient.doInput = true
+                httpClient.useCaches = false
+                httpClient.setRequestProperty("Content-Type","application/json; charset-utf-8")
+            }
+            try{
+                if (params[0] == "POST"){
+                    httpClient.connect()
+                    val os = httpClient.outputStream
+                    val writer = BufferedWriter(OutputStreamWriter(os,"UTF-8"))
+                    writer.write(params[2])
+                    writer.flush()
+                    writer.close()
+                    os.close()
+                }
+                Log.d("e",""+httpClient.responseCode)
+                if(httpClient.responseCode == HttpURLConnection.HTTP_OK){
+                    val stream = BufferedInputStream(httpClient.inputStream)
+                    val data: String = readStream(inputStream = stream)
+                    println("aqui estamos $data")
+
+                    return data
+                } else if((httpClient.responseCode == HttpURLConnection.HTTP_CLIENT_TIMEOUT)){
+                    println("Error${httpClient.responseCode}")
+                }
+                else{
+
+                    println("Error ${httpClient.responseCode}")
+                }
+            }catch(e:Exception){
+                e.printStackTrace()
+            }finally {
+                httpClient.disconnect()
+            }
+            return null
+        }
+
+        fun readStream(inputStream: BufferedInputStream): String{
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val stringBuilder = StringBuilder()
+            bufferedReader.forEachLine { stringBuilder.append(it)}
+            println("aqui estamos ${stringBuilder.toString()}")
+            return stringBuilder.toString()
+        }
+
+        override fun onProgressUpdate(vararg values: Unit?) {
+            super.onProgressUpdate(*values)
+        }
+
+        override fun onPostExecute(result: String?) {
+            if(!result.isNullOrBlank() && !result.isNullOrEmpty()) {
+                val parser: Parser = Parser()
+                val stringBuilder: StringBuilder = StringBuilder(result)
+
+                val json2: JsonObject = parser.parse(stringBuilder) as JsonObject
+                if (json2.int("success") == 1) {
+                    Toast.makeText(applicationContext, "Te has registrado con exito, bienvenido", Toast.LENGTH_SHORT).show()
+                }else if (json2.int("success") == 0){
+                    Toast.makeText(applicationContext, "Ocurrio un problema durante el registro, contacte a sopporte tecnico", Toast.LENGTH_SHORT).show()
+                }
+            }
+            super.onPostExecute(result)
+        }
+
+        override fun onCancelled() {
+            super.onCancelled()
+        }
+    }
+
 }
--- app/src/main/java/com/example/medicalonlineapp/fragments/Agendar.kt
+++ app/src/main/java/com/example/medicalonlineapp/fragments/Agendar.kt
@@ -1,24 +1,34 @@
 package com.example.medicalonlineapp.fragments
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import android.widget.Toast
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
 import com.example.medicalonlineapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_agendar.*
-
+import org.json.JSONObject
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class Agendar : Fragment() {
+
+    val hosting: String = "https://rossworld.000webhostapp.com/"
+    private var progresAsyncTask: ProgressAsyncTask? = null
+    private var fecha:String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
-
-    private fun mostrarMensaje(){
-        Toast.makeText(layoutInflater.context.applicationContext, "este es el mensaje del boton", Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreateView(
@@ -34,11 +44,40 @@ class Agendar : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         agendarCita.setOnClickListener{//aqui dentro es donde pueden ir las llamadas de los botones, los set on click listener y de mas
-            mostrarMensaje()
+            if(!txtNombreAgendar.text.isNullOrEmpty() && txtSexo.selectedItem != ""
+                && ! txtSeguroSocial.text.isNullOrEmpty() && ! txtDomicilio.text.isNullOrEmpty() && ! txtFechaCita.text.isNullOrEmpty()
+                && txtHoraCita.selectedItem != "" && !txtTelefono.text.isNullOrEmpty() && !txtFechaNacimiento.text.isNullOrEmpty()
+                && !txtLocalidad.text.isNullOrEmpty() && !txtObservaciones.text.isNullOrEmpty()) {
+
+                val json = JSONObject()
+                if (txtSexo.selectedItem.toString().equals("Mujer")) {
+                    json.put("Sexo_paciente", 1)
+                } else {
+                    json.put("Sexo_paciente", 0)
+                }
+                json.put("insertCita", true)
+                json.put("Nombre_paciente", txtNombreAgendar.text)
+                json.put("Domicilio_paciente", txtDomicilio.text)
+                json.put("Localidad_paciente", txtLocalidad.text)
+                json.put("Numero_telefono", txtTelefono.text)
+                json.put("Observaciones_paciente", txtObservaciones.text)
+                json.put("Fecha_nacimiento", txtFechaNacimiento.text)
+                json.put("No_seguro_social", txtSeguroSocial.text)
+                json.put("Fecha_cita", txtFechaCita.text)
+                json.put("Hora_cita", txtHoraCita.selectedItem.toString() + ":00")
+                progresAsyncTask = ProgressAsyncTask()
+                progresAsyncTask!!.execute("POST", hosting + "InsertCita.php", json.toString())
+            }else{
+                Toast.makeText(this.requireContext(), "Llene todos los campos e intentelo de nuevo", Toast.LENGTH_SHORT).show()
+            }
         }
 
         txtFechaCita.setOnClickListener{
             showDialogPicker()
+        }
+
+        txtFechaNacimiento.setOnClickListener{
+            showDialogPickerNacimiento()
         }
     }
 
@@ -48,7 +87,113 @@ class Agendar : Fragment() {
     }
 
      private fun onDateSelected(day:Int, month:Int, year:Int){
-        txtFechaCita.setText("$day/${month+1}/$year")
+       txtFechaCita.setText("$year-${month+1}-$day")
+    }
+
+    private fun showDialogPickerNacimiento() {
+        val dataPicker = DatePickerFragment {day, month, year -> onDateSelectedNacimiento(day, month, year)}
+        fragmentManager?.let { dataPicker.show(it,"Fecha de la cita") }
+    }
+
+    private fun onDateSelectedNacimiento(day:Int, month:Int, year:Int){
+        txtFechaNacimiento.setText("$year-${month+1}-$day")
+    }
+
+    inner class ProgressAsyncTask: AsyncTask<String, Unit, String>(){
+
+        val TIMEOUT = 50000
+
+        override fun onPreExecute() {
+            agendarCita.isEnabled = false
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg params: String): String? {
+
+                val url = URL(params[1])
+                val httpClient = url.openConnection() as HttpURLConnection
+                httpClient.readTimeout = TIMEOUT
+                httpClient.connectTimeout = TIMEOUT
+                httpClient.requestMethod = params[0]
+
+                if(params[0] == "POST"){
+                    httpClient.instanceFollowRedirects = false
+                    httpClient.doOutput = true
+                    httpClient.doInput = true
+                    httpClient.useCaches = false
+                    httpClient.setRequestProperty("Content-Type","application/json; charset-utf-8")
+                }
+                try{
+                    if (params[0] == "POST"){
+                        httpClient.connect()
+                        val os = httpClient.outputStream
+                        val writer = BufferedWriter(OutputStreamWriter(os,"UTF-8"))
+                        writer.write(params[2])
+                        writer.flush()
+                        writer.close()
+                        os.close()
+                    }
+                    Log.d("e",""+httpClient.responseCode)
+                    if(httpClient.responseCode == HttpURLConnection.HTTP_OK){
+                        val stream = BufferedInputStream(httpClient.inputStream)
+                        val data: String = readStream(inputStream = stream)
+                        println("aqui estamos $data")
+
+                        val parser: Parser = Parser()
+                        val stringBuilder: StringBuilder = StringBuilder(data)
+
+                        val json2: JsonObject = parser.parse(stringBuilder) as JsonObject
+                        if(json2.int("success") == 1){
+
+                        }
+
+                        return data
+                    } else if((httpClient.responseCode == HttpURLConnection.HTTP_CLIENT_TIMEOUT)){
+                        println("Error${httpClient.responseCode}")
+                    }
+                    else{
+
+                        println("Error ${httpClient.responseCode}")
+                    }
+                }catch(e:Exception){
+                    e.printStackTrace()
+                }finally {
+                    httpClient.disconnect()
+                }
+            return null
+        }
+
+        fun readStream(inputStream: BufferedInputStream): String{
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val stringBuilder = StringBuilder()
+            bufferedReader.forEachLine { stringBuilder.append(it)}
+            println("aqui estamos ${stringBuilder.toString()}")
+            return stringBuilder.toString()
+        }
+
+        override fun onProgressUpdate(vararg values: Unit?) {
+              super.onProgressUpdate(*values)
+        }
+
+        override fun onPostExecute(result: String?) {
+            if(!result.isNullOrBlank() && !result.isNullOrEmpty()) {
+                val parser: Parser = Parser()
+                val stringBuilder: StringBuilder = StringBuilder(result)
+
+                val json2: JsonObject = parser.parse(stringBuilder) as JsonObject
+                if (json2.int("success") == 1) {
+                    Toast.makeText(context, "El registro de su cita se relaizo con exito!", Toast.LENGTH_SHORT).show()
+                }else if (json2.int("success") == 0){
+                    Toast.makeText(context, "Ocurrio un problema durante el registro, contacte a sopporte tecnico", Toast.LENGTH_SHORT).show()
+                }
+            }
+            super.onPostExecute(result)
+            agendarCita.isEnabled = true
+        }
+
+        override fun onCancelled() {
+            super.onCancelled()
+        }
     }
 
 }
